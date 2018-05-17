4620337fa4c9c60005a8260abab230576da6a2c3
Yar Pi <kinglordlear@gmail.com>
2018-05-17 18:30:08 +0000

    Shift Algorithm and CMake Support added

---
+++ Algirithms/Lab5/scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../build
+cmake .. -Dkmp=ON -Dshift=ON
---
+++ Algirithms/Lab5/src/Lab5_SHIFT.cpp
@@ -0,0 +1,18 @@
+#include "algorithm.h"
+
+int main() 
+{
+    string T, P;
+    cin >> P;
+    cin >> T;
+    int i = 0;
+    vector<int> result = KMP_UPGR(T, P);
+    if (result.size() != 0)
+        cout << result[0];
+
+    else
+        cout << "-1";
+    cout << endl;
+    
+    return 0;
+}
