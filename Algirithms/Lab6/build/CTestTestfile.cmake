1987ab88b0293a24ed83facc028a7fe7ba58d34e
ierikhon <kinglordlear@gmail.com>
2018-05-24 18:53:54 +0000

    Some trash deleted

--- Algirithms/Lab6/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/workspace/Labs/Study/Algirithms/Lab6
-# Build directory: /home/ubuntu/workspace/Labs/Study/Algirithms/Lab6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(Lab5_test "./Lab6_jtest")
